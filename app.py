f6d357a56a3e42172d479974f90437572921ee30
bossadip78-dev <bossadip78@gmail.com>
2026-08-29 11:06:57 +0600

    Update app.py

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ def create_bohudur_payment(amount, user_id, username=None):
         cancel_url = f"{base_url}/payment/cancel"
         
         payload = {
-            "full_name": f"User_{user_id}",
+            "full_name": f"BUY-PRODUCT",
             "email": f"user{user_id}@jubayerxtools.com",
             "amount": amount,
             "return_type": "GET",
